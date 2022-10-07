04fbfabd60fa11f901d8f572e32e12aecc630acf
Baran <baran.b.yalcin@live.com>
2022-10-07 09:04:27 +0000

    Added post function and solved some bugs

--- index.js
+++ index.js
@@ -14,16 +14,32 @@ app.get("/create", function(req, res) {
   res.sendFile("/create.html", { root: "." });
 });
 
+app.post('/create', function(req, res, next) {
+  client.connect(err => {
+    const dancers = client.db("drmdb").collection("dancers");
+
+    let dancer = { name: req.body.name, surname: req.body.surname, telephone: req.body.telephone, branch: req.body.branch, attandance: req.body.attandance };
+
+    dancers.insertOne(dancer, function(err, res) {
+      if (err) throw err;
+      console.log("1 dancer added");
+    });
+
+  })
+  res.send("Dancer Created");
+})
+
 app.set("port", process.env.PORT || 5000);
 http.listen(app.get("port"), function() {
   console.log("listening on port", app.get("port"));
 });
 
+
 const MongoClient = require("mongodb").MongoClient;
 const mongo_username = process.env.MONGO_USERNAME;
 const mongo_password = process.env.MONGO_PASSWORD;
 
-const uri = `mongodb+srv://<mongo_username>:<mongo_password>@mongodeneme.1n0pcdf.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@mongodeneme.1n0pcdf.mongodb.net/drmdb?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
